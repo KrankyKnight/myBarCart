51d3aa818b8681eb51c72730dd85fa2a03f1a0cc
Donald Twiford <donaldptwiford@gmail.com>
2023-06-06 16:42:15 -0700

    Successfully render app

--- client/index.js
+++ client/index.js
@@ -8,16 +8,16 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import store from './store';
+// import store from './store';
 //styles import
 
-const root = createRoot(document.getElementById('root'));
+console.log('in index.js')
 
+const root = createRoot(document.getElementById('root'));
 console.log('starting');
 
 root.render(
-    <Provider store={store}>
+    /*<Provider store={store}>*/
         <App/>
-    </Provider>,
-    document.getElementById('root'),
+    /*</Provider>*/
 )
