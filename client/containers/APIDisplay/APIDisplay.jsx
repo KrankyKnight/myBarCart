550c0cf4815193cf9b2ea7442d5a38bfe6f79d4c
Donald Twiford <donaldptwiford@gmail.com>
2024-05-01 16:01:12 -0700

    Clean up code

--- client/containers/APIDisplay/APIDisplay.jsx
+++ client/containers/APIDisplay/APIDisplay.jsx
@@ -19,6 +19,7 @@ const APIDisplay = () => {
 
   const viewOutput = [];
   let ingredientCount = 0;
+
   if(viewMode === 'ingredients'){
     if(searchText.length === 0) {
       for(const ingredient of ingredientList) {
@@ -36,6 +37,7 @@ const APIDisplay = () => {
 
   const recipeCards = [];
   let recipeCount = 0;
+
   if(viewMode === 'recipes' && recipes.length){
     for(let recipe of recipes) {
       const { id, name, content, glass, instructions, image, ingredients } = recipe;
@@ -46,30 +48,30 @@ const APIDisplay = () => {
 
   return(
     <div id='APIDisplay' className={`api ${viewMode}`}>
-      {viewMode === 'none' ? 
-        <p className='blurb'>
-          <h4>Welcome to myBarCart</h4> 
-          <div>This app brought to you thanks to the wonderful devs at <em>thecocktailDB</em></div>
-          <div>Visit their website at <a href='https://www.thecocktaildb.com/'>www.thecocktaildb.com/</a></div>
-          <div>And support them on their patreon at <a href='https://www.patreon.com/thedatadb'>www.patreon.com/thedatadb</a></div>
-        </p>
-      : viewMode === 'ingredients' ? 
-        <ul id='ingredientList'>
-          {viewOutput}
-        </ul>
-      :viewMode === 'recipes' && recipes === 'pending' ? 
-        <span className='blurb'>Fetching your recipes. Please be patient, API's are expensive and I am cheap. Pinging one... alot.</span>
-      :viewMode === 'recipes' && !recipes.length ?
-        <span className='blurb'>Sadly there are no recipes in the database that fully contain your cart. Time to go shopping... or do shots. No judgement.</span>
-      :viewMode === 'recipes' && recipes.length ? 
-        recipeCards
-      :
-        <div>
-          <span className='blurb'>This should not be displaying. Shhh... don't tell anyone...</span>
-          <span className='blurb'>Something is not right... good luck code man!</span>
-        </div>
+      {
+        viewMode === 'none' ? 
+          <p className='blurb'>
+            <h4>Welcome to myBarCart</h4> 
+            <div>This app brought to you thanks to the wonderful devs at <em>thecocktailDB</em></div>
+            <div>Visit their website at <a href='https://www.thecocktaildb.com/'>www.thecocktaildb.com/</a></div>
+            <div>And support them on their patreon at <a href='https://www.patreon.com/thedatadb'>www.patreon.com/thedatadb</a></div>
+          </p>
+        : viewMode === 'ingredients' ? 
+          <ul id='ingredientList'>
+            {viewOutput}
+          </ul>
+        :viewMode === 'recipes' && recipes === 'pending' ? 
+          <span className='blurb'>Fetching your recipes. Please be patient, API's are expensive and I am cheap. Pinging one... alot.</span>
+        :viewMode === 'recipes' && !recipes.length ?
+          <span className='blurb'>Sadly there are no recipes in the database that fully contain your cart. Time to go shopping... or do shots. No judgement.</span>
+        :viewMode === 'recipes' && recipes.length ? 
+          recipeCards
+        :
+          <div>
+            <span className='blurb'>This should not be displaying. Shhh... don't tell anyone...</span>
+            <span className='blurb'>Something is not right... good luck code man!</span>
+          </div>
       }
-
     </div>
   );
 };
